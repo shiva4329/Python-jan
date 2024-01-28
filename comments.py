b44a29ea97ec4f55323c8bca1f17ebcaeae2b72d
shiva_4329 <sambasivaraokandi@gmail.com>
2024-01-28 20:51:23 +0530

    comments in python

---
+++ comments.py
@@ -0,0 +1,17 @@
+# comments : it will use to comment the line or lines of code those are no need to execute
+    # types:
+            # single comment : # ---> used for sing line comment
+            # Multiline comment : '''     ''' -----> used for Multi line comment
+
+
+x = 10
+y = 20
+z = 30
+a = 40
+
+#print(x) # single ine comment
+print(y)
+
+# multi line comment
+'''print(z)
+print(a)'''
