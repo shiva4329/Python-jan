8890af4e53e2b9bb0252b558401dc688f4315751
shiva_4329 <sambasivaraokandi@gmail.com>
2024-02-18 23:28:55 +0530

    feb 17

---
+++ source/real_time_input.py
@@ -0,0 +1,24 @@
+# real time input ----> it takes input from user for that we need to use input() fn
+
+x = 50
+y = 20
+
+x = input() # default input returns str type
+print("x value is ",x)
+print(type(x))
+
+# x = 10
+# y = 30
+
+# print(x+y)
+
+# input as integer need to use int() fn befor syntax: int(input(value...))
+
+x = int(input()) # converting integer
+y = int(input())
+
+print(x+y)
+
+# int(input()) for int
+# float(input()) for float
+# bool(input()) for bool
