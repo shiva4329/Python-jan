617542bcf241a9a89947ffea7c046dc40cac7e3e
shiva_4329 <sambasivaraokandi@gmail.com>
2024-03-12 07:55:04 +0530

    Mar 12

--- source/ControlStructres/loops/Comprehensions.py
+++ source/ControlStructres/loops/Comprehensions.py
@@ -2,7 +2,7 @@
 # list : syntax : [<var> for-loop <conditions/expressions>]
 # tuple
 # set
-# dict
+# dict : syntax : {key:value for i in <variable/fn> <condition>}
 
 
 ip = 10
@@ -25,3 +25,38 @@ print(x)
 x = [i for i in range(1,ip+1) if i%2 == 0]
 
 print(x)
+
+
+# tuple comprehension
+
+vals = range(10)
+
+y = (i for i in vals)
+
+print('tuple : ',y)
+
+# Note : tuple comprehension generates indirect object
+
+
+# set comprehension
+val = range(10)
+a = {i for i in val }
+
+print(a,type(a))
+
+
+# dictionary comprehension : syntax : {key:value for i in <variable/fn> <condition>}
+
+dictionary = {i:i for i in range(10)}
+
+print(dictionary)
+
+# create a dictionry using two lists
+
+k = ['a','b','c']
+v = [10,20,30]
+
+print(zip(k,v)) # generates indirect object it holds : a,10,b,20,c,30
+
+z = {i:j for i,j in zip(k,v)}
+print(z)
