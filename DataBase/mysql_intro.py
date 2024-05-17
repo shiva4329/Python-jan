322a66e1e56fdc4624c700705c3a21d87e1ed187
shiva_4329 <sambasivaraokandi@gmail.com>
2024-05-17 07:39:16 +0530

    MAY 17

---
+++ DataBase/mysql_intro.py
@@ -0,0 +1,49 @@
+# DataBase : where we can store the data
+
+# types : 2
+#          1. relational database
+#            type of data :   data is in table format(rows/columns)
+#                  Ex. MySql
+#          2. non-relational database
+#               type of data :  document(dictionary)
+#                  Ex. NoSQl(MongoDB)
+
+
+# MySQl : it is one of the popular relational database
+
+# datatypes:
+#           int
+#           varchar(255) ---> max length
+#           varchar(20) ----> particular length---> 20 char
+
+# Creating data base
+#     create database <db name>;
+
+# use the created database / particular data
+
+#          use <db name>;
+
+# check the available tables in database
+
+#       show tables;
+
+# create table in databse
+
+#  create table <table name>(col1 dtype,col2 dtype......);
+
+
+# inserting data into table
+
+#   insert into Student(id,name,age,address,mobile) values(1,'miller',34,'US','012345678');
+
+# show data
+
+# select * from <table name>
+
+# select * from Student where age = 34 and address = 'US';
+#
+# select * from Student where age > 30;
+#
+# select name from Student;
+#
+# select name,age from Student;
---
+++ source/OOP's/exercises/company.py
@@ -0,0 +1,9 @@
+
+# create Company class
+# define variables
+#   Cname,Cadd,sal
+# create options method
+# create HRA method
+#       sal = (sal*0.15) + sal
+# create PF method
+#       sal = (sal*0.12) - sal
