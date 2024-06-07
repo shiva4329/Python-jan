09de2e73e2f8858617b6ee0b677a900910cfdafc
shiva_4329 <sambasivaraokandi@gmail.com>
2024-06-07 06:48:12 +0530

    june 07

--- DataBase/Excercise/pymysql_exercise.py
+++ DataBase/Excercise/pymysql_exercise.py
@@ -10,26 +10,22 @@
 
 
 # code
-
-# importing librarys
+# importing library's
 import pandas as pd
 import pymysql as pm
 
 
 # reads the file
 df = pd.read_excel('/home/siva/Desktop/student_details.xls')
-
 print(df)
 
 # connect to the mysql db
 con = pm.connect(user = 'root', password= 'Root', host= 'localhost',db='School')
 print(con)
-
-cur = con.cursor()
+cur = con.cursor() # creating cursor
 
 # extracting the columns from file data
 df_cols = []
-
 for i in df.columns:# reading cols from df
     r = i.replace(' ','') #removes empty sp[ace from col name
     df_cols.append(f'{r} varchar(50)') # adding datatype for col name
@@ -37,10 +33,15 @@ for i in df.columns:# reading cols from df
 x = ','.join(df_cols) #removes quote(') from string
 
 # creating table using columns
-query = f"create table Students({x})"
-
+# note : if table is already available in mySql no need run create command, if not only need to run create command
+query = f"create table Students({x})" # generating table creation query
 print(query)
-# cur.execute(query)
+cur.execute(query) # if table is not exist
 
+# reading data from file row by row and inserting
 for i,row in df.iterrows():
-    print(row['Age'],row['Id'])
+    row = [str(i) for i in list(row)] # extracting rows from file
+    query = f'Insert into Students values{tuple(row)}'# generating insert query
+    print(query)
+    cur.execute(query) # executing insert command
+    con.commit()# savinf data into MySQL
